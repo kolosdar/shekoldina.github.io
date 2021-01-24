c31038b04764c8645320289afee50e9e9b0c7fd8
shekoldina <49490661+shekoldina@users.noreply.github.com>
2021-01-24 17:35:53 +0300

    Delete photography directory

--- photography/js/linkActive.js
+++
@@ -1,7 +0,0 @@
-var menu__items = document.getElementsByClassName("menu__link");
-var url = document.location.href;
-for (var i = 0; i < menu__items.length; i++) {
-	if (url == menu__items[i].href) {
-		menu__items[i].classList.add('menu__link_active');
-	}
-}
